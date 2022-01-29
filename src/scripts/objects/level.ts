e49fe0d6d4326a3ce0905e57136e44e687c6e2eb
Michael Main <deusprogrammer@gmail.com>
2022-01-29 13:19:18 -0600

    Fixed a few small problems

--- src/scripts/objects/level.ts
+++ src/scripts/objects/level.ts
@@ -23,11 +23,13 @@ export default class Level extends Phaser.GameObjects.GameObject {
 		for (let y = 0; y < this.levelConfig.blocksY; y++) {
 			for (let x = 0; x < this.levelConfig.blocksX; x++) {
 				if (this.levelConfig.tilemap[y][x] == "*") {
+					console.log("BLOCK:  " + (x * config.BLOCK_SIZE) + ", " + (y * config.BLOCK_SIZE));
 					let block = this.blocks.create(x * config.BLOCK_SIZE, y * config.BLOCK_SIZE, this.levelConfig.blockAsset);
 					this.scene.physics.add.existing(block);
 					block.body.setSize(config.BLOCK_SIZE - config.BB_ADJUST, config.BLOCK_SIZE - config.BB_ADJUST, config.BB_ADJUST / 2, - config.BB_ADJUST / 2);
 					block.body.immovable = true;
 				} else if (this.levelConfig.tilemap[y][x] == "#") {
+					console.log("MBLOCK: " + (x * config.BLOCK_SIZE) + ", " + (y * config.BLOCK_SIZE));
 					let block = this.moveable.create(x * config.BLOCK_SIZE, y * config.BLOCK_SIZE, this.levelConfig.breakableAsset);
 					this.breakableBlocks[x + "," + y] = block;
 					this.scene.physics.add.existing(block);
@@ -38,7 +40,7 @@ export default class Level extends Phaser.GameObjects.GameObject {
 		}
 	}
 
-	getTile(x: number, y: number) : boolean {
+	getTile(x: number, y: number) : string {
 		return this.levelConfig.tilemap[y][x];
 	}
 
